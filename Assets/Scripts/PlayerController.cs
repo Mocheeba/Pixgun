533ce39fdee8b17870b3cca35e0a45d520802d44
Marcel Kolodziej <kolodziejmarcel1@gmail.com>
2022-04-02 05:55:59 +0200

    Dodanie mapki
    Dodanie mapki do parallaxu

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,153 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private float movementInputDirection;
+
+    private int amountOfJumpsLeft;
+
+    private bool isFacingRight = true;
+    private bool isWalking = true;
+    private bool isGrounded;
+    private bool canJump;
+
+    private Rigidbody2D rb;
+    private Animator anim;
+
+    public int amountOfJumps = 1;
+
+    public float movementSpeed = 10.0f;
+    public float jumpForce = 10.0f;
+    public float groundCheckRadius;
+
+
+    public ParticleSystem dust;
+
+    public Transform groundCheck;
+
+    public LayerMask whatIsGround;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        amountOfJumpsLeft = amountOfJumps;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckInput();
+        CheckMovementDirection();
+        UpdateAnimations();
+    }
+
+    private void FixedUpdate()
+    {
+        ApplyMovement();
+        CheckSurroudings();
+        UpdateAnimations();
+        CheckIfCanJump();
+    }
+
+    private void CheckGround()
+    {
+
+    }
+
+    private void CheckSurroudings()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+    }
+
+    private void CheckIfCanJump()
+    { 
+        if(isGrounded &&  rb.velocity.y <= 0)
+        {
+            amountOfJumpsLeft = amountOfJumps;
+        }    
+
+        if(amountOfJumpsLeft <= 0)
+        {
+            canJump = false;
+        }
+        else
+        {
+            canJump = true;
+        }
+    }
+
+    private void CheckMovementDirection()
+    {
+        if(isFacingRight && movementInputDirection < 0)
+        {
+            Flip();
+        }
+        else if (!isFacingRight && movementInputDirection > 0)
+        {
+            Flip();
+        }
+
+        if(Mathf.Abs(rb.velocity.x) >= 0.01f)
+        {
+            isWalking = true;
+        }
+        else
+        {
+            isWalking = false;
+        }
+    }
+
+    private void UpdateAnimations()
+    {
+        anim.SetBool("isWalking", isWalking);
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetFloat("yVelocity", rb.velocity.y);
+    }
+
+    private void CheckInput()
+    {
+        movementInputDirection = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    }
+
+    private void ApplyMovement()
+    {
+        rb.velocity = new Vector2(movementSpeed * movementInputDirection, rb.velocity.y);
+    }
+
+    private void Flip()
+    {
+        CreateDust();
+        isFacingRight = !isFacingRight;
+        transform.Rotate(0.0f, 180.0f, 0.0f);
+    }
+
+    private void Jump()
+    {
+        if(canJump)
+        {
+            CreateDust();
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            amountOfJumpsLeft--;
+        }
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+    }
+
+    void CreateDust()
+    {
+        dust.Play();
+    }
+}
