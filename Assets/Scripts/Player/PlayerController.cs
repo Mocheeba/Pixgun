d74a7ec323d1a476ad45e32b56714db5927f0317
Marcel Kolodziej <kolodziejmarcel1@gmail.com>
2022-04-11 20:45:20 +0200

    Working on attack
    Working on attack - adding events

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -137,7 +137,7 @@ public class PlayerController : MonoBehaviour
             Flip();
         }
 
-        if (rb.velocity.x != 0)
+        if (Mathf.Abs(rb.velocity.x) > 0.01f)
         {
             isWalking = true;
         }
@@ -295,6 +295,16 @@ public class PlayerController : MonoBehaviour
                 rb.velocity = new Vector2(rb.velocity.x, -wallSlideSpeed);
             }
         }
+    }
+
+    public void DisableFlip()
+    {
+        canFlip = false;
+    }
+
+    public void EnableFlip()
+    {
+        canFlip = true;
     }
 
     private void Flip()
