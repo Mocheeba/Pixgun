1b771333653951f8e7edba786e4ebff05d179cae
Marcel Kolodziej <kolodziejmarcel1@gmail.com>
2022-04-21 23:40:09 +0200

    Setting up player
    Start Finite Machines, WORK

--- Assets/Scripts/Player/Input/PlayerInputHandler.cs
+++ Assets/Scripts/Player/Input/PlayerInputHandler.cs
@@ -5,13 +5,27 @@ using UnityEngine.InputSystem;
 
 public class PlayerInputHandler : MonoBehaviour
 {
+    private Vector2 movementInput;
     public void OnMoveInput(InputAction.CallbackContext context)
     {
-
+        movementInput = context.ReadValue<Vector2>();
+        Debug.Log("On movement Input");
     }
 
-    public void OnInputJump(InputAction.CallbackContext context)
+    public void OnJumpInput(InputAction.CallbackContext context)
     {
-
+        Debug.Log("On jump input");
+        if (context.started)
+        {
+            Debug.Log("Jump button pushed down now");
+        }
+        if (context.performed)
+        {
+            Debug.Log("Jump is beign held down");
+        }
+        if (context.canceled)
+        {
+            Debug.Log("Jump button has been released");
+        }
     }
 }
