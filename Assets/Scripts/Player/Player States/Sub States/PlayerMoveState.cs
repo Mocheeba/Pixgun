85bbed8662b9c5a78eabc8b29a039aa247cdaa4a
Marcel Kolodziej <kolodziejmarcel1@gmail.com>
2022-04-22 04:08:37 +0200

    WORKS
    przed Jumpem, nowe chodzenie działa

--- Assets/Scripts/Player/Player States/Sub States/PlayerMoveState.cs
+++ Assets/Scripts/Player/Player States/Sub States/PlayerMoveState.cs
@@ -27,6 +27,8 @@ public class PlayerMoveState : PlayerGroundedState
     {
         base.LogicUpdate();
 
+        player.CheckIfshouldFlip(xInput);
+
         player.SetVelocityX(playerData.movementVelocity * xInput);
 
         if (xInput == 0)
--- Assets/Scripts/Player/PlayerFiniteStateMachine/Player.cs
+++ Assets/Scripts/Player/PlayerFiniteStateMachine/Player.cs
@@ -4,23 +4,28 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    #region State Variables
     public PlayerStateMachine StateMachine { get; private set; }
 
     public PlayerIdleState IdleState { get; private set; }
     public PlayerMoveState MoveState { get; private set; }
-
+    [SerializeField]
+    private PlayerData PlayerData;
+    #endregion
+    #region Components
     public Animator Anim { get; private set; }
     public PlayerInputHandler InputHandler { get; private set; }
     public Rigidbody2D RB { get; private set; }
-
+    #endregion
+    #region Other variables
     public Vector2 CurrentVelocity { get; private set; }
-
-    [SerializeField]
-    private PlayerData PlayerData;
-
+    
+    public int facingDirection {  get; private set; }
     private Vector2 workspace;
 
+    #endregion
 
+    #region Unity Callback Functions
     private void Awake()
     {
         StateMachine = new PlayerStateMachine();
@@ -31,6 +36,8 @@ public class Player : MonoBehaviour
 
     private void Start()
     {
+        facingDirection = 1;
+
         Anim = GetComponent<Animator>();
         InputHandler = GetComponent<PlayerInputHandler>();
         RB = GetComponent<Rigidbody2D>();
@@ -49,11 +56,29 @@ public class Player : MonoBehaviour
     {
         StateMachine.CurrentState.PhysicsUpdate();
     }
-
+    #endregion
+    #region Set Functions
     public void SetVelocityX(float velocity)
     {
         workspace.Set(velocity, CurrentVelocity.y);
         RB.velocity = workspace;
         CurrentVelocity = workspace;
     }
+    #endregion
+    #region Check Functions
+    public void CheckIfshouldFlip(int xInput)
+    {
+        if(xInput != 0 && xInput != facingDirection)
+        {
+            Flip();
+        }
+    }
+    #endregion
+#region Other Functions
+    private void Flip()
+    {
+        facingDirection *= -1;
+        transform.Rotate(0.0f, 180.0f, 0.0f);
+    }
+    #endregion
 }
