79e512ecfd756146cddc6cdbbfcda40cd90825f4
Marcel Kolodziej <kolodziejmarcel1@gmail.com>
2022-04-22 03:11:33 +0200

    działa bez dodania vectora
    przed dodaniem vectora

--- Assets/Scripts/Player/Player States/Sub States/PlayerMoveState.cs
+++ Assets/Scripts/Player/Player States/Sub States/PlayerMoveState.cs
@@ -7,4 +7,34 @@ public class PlayerMoveState : PlayerGroundedState
     public PlayerMoveState(Player player, PlayerStateMachine stateMachine, PlayerData playerData, string animBoolName) : base(player, stateMachine, playerData, animBoolName)
     {
     }
+
+    public override void DoChecks()
+    {
+        base.DoChecks();
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+
+        if(input.x == 0f)
+        {
+            stateMachine.ChangeState(player.IdleState);
+        }
+    }
+
+    public override void PhysicsUpdate()
+    {
+        base.PhysicsUpdate();
+    }
 }
