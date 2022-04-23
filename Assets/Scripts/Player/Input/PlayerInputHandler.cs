c0044d08eef6a274ae70947fa44fc1f5a9be0cf5
Marcel Kolodziej <kolodziejmarcel1@gmail.com>
2022-04-23 23:03:56 +0200

    Player Controller

--- Assets/Scripts/Player/Input/PlayerInputHandler.cs
+++ Assets/Scripts/Player/Input/PlayerInputHandler.cs
@@ -10,6 +10,18 @@ public class PlayerInputHandler : MonoBehaviour
     public int NormInputX { get; private set; }
     public int NormInputY { get; private set; }
     public bool JumpInput { get; private set; }
+
+    [SerializeField]
+    private float imputHoldTime = 0.2f;
+
+    private float jumpInputStartTime;
+
+    private void Update()
+    {
+        CheckJumpInputHoldTime();
+    }
+
+
     public void OnMoveInput(InputAction.CallbackContext context)
     {
         RawMovementInput = context.ReadValue<Vector2>();
@@ -24,8 +36,18 @@ public class PlayerInputHandler : MonoBehaviour
        if(context.started)
         {
             JumpInput = true;
+            jumpInputStartTime = Time.time;
         }
     }
 
     public void UseJumpInput() => JumpInput = false;
+
+    private void CheckJumpInputHoldTime()
+    {
+        if(Time.time >= jumpInputStartTime + imputHoldTime)
+        {
+            JumpInput = false;
+        }
+    }
+
 }
