0d75d99c77afd6a55a433bf401dcead7677ccc10
Marcel Kolodziej <kolodziejmarcel1@gmail.com>
2022-05-06 20:08:56 +0200

    skakanie od krawędzi

--- Assets/Scripts/Player/Player States/Sub States/PlayerAirState.cs
+++ Assets/Scripts/Player/Player States/Sub States/PlayerAirState.cs
@@ -97,7 +97,7 @@ public class PlayerInAirState : PlayerState
         {
             stateMachine.ChangeState(player.JumpState);
         }
-        else if(isTouchingWall && grabInput)
+        else if(isTouchingWall && grabInput && isTouchingLedge)
         {
             stateMachine.ChangeState(player.WallGrabState);
         }
