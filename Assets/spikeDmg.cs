1fe613d7a667aa57d147492c6745cc922961e635
Marcel Kolodziej <kolodziejmarcel1@gmail.com>
2022-05-10 00:15:44 +0200

    b4 jumping

--- Assets/spikeDmg.cs
+++ Assets/spikeDmg.cs
@@ -8,7 +8,7 @@ public class spikeDmg : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
+        if (collision.tag == "Player") 
             collision.GetComponent<Health>().TakeDamage(damage);
     }
 }
