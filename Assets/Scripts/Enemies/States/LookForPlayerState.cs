00cc7f99bade2697df1540239bda280f5b692f37
Marcel Kolodziej <kolodziejmarcel1@gmail.com>
2022-06-11 00:41:44 +0200

    lookFor player

---
+++ Assets/Scripts/Enemies/States/LookForPlayerState.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LookForPlayerState : State
+{
+    protected D_LookForPlayer stateData;
+
+    protected bool turnImmediately;
+    protected bool isPlayerInMinAgroRange;
+    protected bool isAllTurnsDone;
+    protected bool isAllTurnsTimeDone;
+
+    protected float lastTurnTime;
+
+    protected int amountOfTurnsDone;
+
+    public LookForPlayerState(Entity etity, FiniteStateMachine stateMachine, string animBoolName, D_LookForPlayer stateData) : base(etity, stateMachine, animBoolName)
+    {
+        this.stateData = stateData;
+    }
+
+    public override void DoChecks()
+    {
+        base.DoChecks();
+
+        isPlayerInMinAgroRange = entity.CheckPlayerInMinAgroRange();
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+
+        if(turnImmediately)
+        {
+            entity.Flip();
+            lastTurnTime = Time.time;
+            amountOfTurnsDone++;
+            turnImmediately = false;
+
+        }
+        else if(Time.time >= lastTurnTime + stateData.timeBetweenTurns && !isAllTurnsDone)
+        {
+            entity.Flip();
+            lastTurnTime = Time.time;
+            amountOfTurnsDone++;
+        }
+    }
+
+    public override void PhysicsUpdate()
+    {
+        base.PhysicsUpdate();
+    }
+
+    public void SetTurnImmediately(bool flip)
+    {
+        turnImmediately = flip;
+    }
+}
