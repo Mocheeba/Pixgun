8bbca89fdd847fb1d2182e02827855e1e0618150
Marcel Kolodziej <kolodziejmarcel1@gmail.com>
2022-06-11 02:21:20 +0200

    plater

--- Assets/Scripts/Enemies/EnemySpecific/Enemy1/E1_LookForPlayerState.cs
+++ Assets/Scripts/Enemies/EnemySpecific/Enemy1/E1_LookForPlayerState.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class E1_LookForPlayerState : State
 {
+    public E1_LookForPlayerState(Entity etity, FiniteStateMachine stateMachine, string animBoolName) : base(etity, stateMachine, animBoolName)
+    {
+    }
+
     // Start is called before the first frame update
     void Start()
     {
