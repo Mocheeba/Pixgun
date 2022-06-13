3eeb88bd5b446f7677448a5796eaa3c7a80766fc
Marcel Kolodziej <kolodziejmarcel1@gmail.com>
2022-06-13 02:41:15 +0200

    Dummy

--- Assets/Scripts/Interface/AggresiveWeapon.cs
+++ Assets/Scripts/Interface/AggresiveWeapon.cs
@@ -4,8 +4,35 @@ using UnityEngine;
 
 public class AggresiveWeapon : Weapon
 {
+    private List<IDamageable> detectedDamageable = new List<IDamageable>();
  public override void AnimationActionTrigger()
     {
         base.AnimationActionTrigger();
     }    
+
+    public void AddToDetected(Collider2D collision)
+    {
+        IDamageable damageable = collision.GetComponent<IDamageable>();
+
+        if (damageable != null)
+        {
+            Debug.Log("Added");
+
+            detectedDamageable.Add(damageable);
+        }
+    }
+
+    public void RemoveFromDetected(Collider2D collision)
+    {
+        Debug.Log("Remove from Detected");
+
+        IDamageable damageable = collision.GetComponent<IDamageable>();
+
+        if (damageable != null)
+        {
+            Debug.Log("Removed");
+
+            detectedDamageable.Remove(damageable);
+        }
+    }
 }
