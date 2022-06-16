9b823df55bba297303beac67e14342c7b278f413
Marcel Kolodziej <kolodziejmarcel1@gmail.com>
2022-06-16 06:16:16 +0200

    praca przeciwnik

--- Assets/meleeEnemy.cs
+++ Assets/meleeEnemy.cs
@@ -14,23 +14,34 @@ public class meleeEnemy : MonoBehaviour
 
 
     private Animator anim;
+    private Health playerHealth;
+
+
+    private EnemyPatrol EnemyPatrol;
+
 
 
     private void Awake()
-    {
+    {   
         anim = GetComponent<Animator>();
+        EnemyPatrol = GetComponentInParent<EnemyPatrol>();
     }
 
     private void Update()
     {
         cooldownTimer += Time.deltaTime;
 
-        if (cooldownTimer >= attackCooldown)
+        if(PlayerInSight())
         {
-            cooldownTimer = 0;
-            anim.SetBool("meleeAttack", true);
+            if (cooldownTimer >= attackCooldown)
+            {
+                cooldownTimer = 0;
+                anim.SetTrigger("meleeAttack");
+            }
         }
 
+        if(EnemyPatrol != null)
+           EnemyPatrol.enabled = !PlayerInSight();
     }
 
     private bool PlayerInSight()
@@ -40,13 +51,25 @@ public class meleeEnemy : MonoBehaviour
             new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z),
             0, Vector2.left, 0, playerLayer);
 
+        if(hit.collider != null)
+            playerHealth = hit.transform.GetComponent<Health>();
+
         return hit.collider != null;
     }
+
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireCube(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
             new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z));
+    }
+
+
+    private void DamagePlayer()
+    {
+        if (PlayerInSight())
+            playerHealth.TakeDamage(damage);
+
     }
 
 }
