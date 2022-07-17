8eb983920af9514eb39f3176df85a1bf59029e3b
Marcel Kolodziej <kolodziejmarcel1@gmail.com>
2022-07-17 23:54:59 +0200

    Staet Machine Setup
    Working

--- Assets/Scripts/Player/PlayerFiniteStateMachine/Player.cs
+++ Assets/Scripts/Player/PlayerFiniteStateMachine/Player.cs
@@ -4,23 +4,34 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    #region State Variables
     public PlayerStateMachine StateMachine { get; private set; }
 
     public PlayerIdleState IdleState {get; private set; }
     public PlayerMoveState MoveState { get; private set; }
-
-    public Animator Anim {get; private set; }
-
+    #endregion
+    #region Components
     public PlayerInputHandler InputHandler { get; private set; }
+    public Rigidbody2D RB { get; private set; }
+    public Animator Anim { get; private set; }
 
+    #endregion
+    #region Other Variables
+
+
+    public Vector2 CurrentVelocity { get; private set; }
+    public int FacingDirection { get; private set; }
     [SerializeField]
     private PlayerData playerData;
 
+    private Vector2 workSpace;
+    #endregion
+    #region Unity Callback Functions
     public void Awake()
     {
         StateMachine = new PlayerStateMachine();
 
-        IdleState = new PlayerIdleState(this, StateMachine, playerData, "idle" );
+        IdleState = new PlayerIdleState(this, StateMachine, playerData, "idle");
         MoveState = new PlayerMoveState(this, StateMachine, playerData, "move");
     }
 
@@ -28,12 +39,16 @@ public class Player : MonoBehaviour
     {
         Anim = GetComponent<Animator>();
         InputHandler = GetComponent<PlayerInputHandler>();
+        RB = GetComponent<Rigidbody2D>();
+
+        FacingDirection = 1;
 
         StateMachine.Initialize(IdleState);
     }
 
     private void Update()
     {
+        CurrentVelocity = RB.velocity;
         StateMachine.CurrentState.LogicUpdate();
     }
 
@@ -41,4 +56,29 @@ public class Player : MonoBehaviour
     {
         StateMachine.CurrentState.PhysicsUpdate();
     }
+    #endregion
+    #region Set Functions
+    public void SetVelocityX(float velocity)
+    {
+        workSpace.Set(velocity, CurrentVelocity.y);
+        RB.velocity = workSpace;
+        CurrentVelocity = workSpace;
+    }
+    #endregion
+    #region Check Functions
+    public void CheckIfShouldFlip(int xInput)
+    {
+        if (xInput != 0 && xInput != FacingDirection)
+        {
+            Flip();
+        }
+    }
+    #endregion
+    #region Other Functions
+    private void Flip()
+    {
+        FacingDirection *= -1;
+        transform.Rotate(0.0f, 180.0f, 0.0f);
+    }
+    #endregion
 }
