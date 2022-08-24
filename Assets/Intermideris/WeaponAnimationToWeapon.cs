e03ddebcd728b11a56a6d7830fe0412d27ebb3c5
Marcel Kołodziej <kolodziejmarcel1@gmail.com>
2022-08-24 19:37:04 +0200

    flip dziala

--- Assets/Intermideris/WeaponAnimationToWeapon.cs
+++ Assets/Intermideris/WeaponAnimationToWeapon.cs
@@ -25,4 +25,14 @@ public class WeaponAnimationToWeapon : MonoBehaviour
     {
         weapon.AnimationStopMovementTrigger();
     }
+
+    private void AnimationTurnOffFlip()
+    {
+        weapon.AnimationTurnOffFlip();
+    }
+
+    private void AnimationTurnOnFlip()
+    {
+        weapon.AnimationTurnOnFlip();
+    }
 }
