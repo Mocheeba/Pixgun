36762b8477d1095992599dfe787b1854a74b386c
Marcel Kołodziej <kolodziejmarcel1@gmail.com>
2022-09-23 12:00:35 +0200

    IEnumerator

--- Assets/HealthScripts/Health.cs
+++ Assets/HealthScripts/Health.cs
@@ -1,11 +1,21 @@
 using UnityEngine;
+using System.Collections;
+
 
 public class Health : MonoBehaviour
 {
+    [Header ("Health")]
+    
     [SerializeField] private float startingHealth;
     public float currentHealth { get; private set; }
-  //  private Animator anim;
-    //private bool dead;
+    private Animator anim;
+    public Rigidbody2D RB { get; private set; }
+    private bool dead;
+
+    [Header ("iFrames")]
+    [SerializeField] private float iFramesDuration;
+    [SerializeField] private int numberOfFlashes;
+    private SpriteRenderer spriteRend;
 
     private void Update()
      {
@@ -17,7 +27,9 @@ public class Health : MonoBehaviour
     private void Awake()
     {
         currentHealth = startingHealth;
-      //  anim = GetComponent<Animator>();
+        anim = GetComponent<Animator>();
+        RB = GetComponentInParent<Rigidbody2D>();
+        spriteRend = GetComponent<SpriteRenderer>();
     }
     public void TakeDamage(float _damage)
     {
@@ -25,21 +37,34 @@ public class Health : MonoBehaviour
 
         if (currentHealth > 0)
         {
-          //  anim.SetTrigger("hurt");
-            //iframes
+          anim.SetTrigger("hurt");
+            StartCoroutine(Invunerability());
         }
         else
         {
-            // if (!dead)
-            // {
-            //     anim.SetTrigger("die");
-            //     GetComponent<PlayerMovement>().enabled = false;
-            //     dead = true;
-            // }
+             if (!dead)
+             {
+                 anim.SetTrigger("die");
+                 
+                 dead = true;
+             }
         }
     }
-    // public void AddHealth(float _value)
-    // {
-    //     currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
-    // }
+     public void AddHealth(float _value)
+     {
+         currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
+     }
+
+    private IEnumerator Invunerability()
+    {
+        Physics2D.IgnoreLayerCollision(7, 8, true);
+        for (int i = 0; i < numberOfFlashes; i++)
+        {
+            spriteRend.color = new Color(1, 0, 0, 0.5f);
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+            spriteRend.color = Color.white;
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+        }
+        Physics2D.IgnoreLayerCollision(7, 8, false);
+    }
 }
