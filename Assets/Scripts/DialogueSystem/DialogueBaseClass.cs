07c06c2289d9cc0fba6659137633c96ac6a25564
Marcel Kołodziej <kolodziejmarcel1@gmail.com>
2022-09-25 01:35:31 +0200

    Dialog

--- Assets/Scripts/DialogueSystem/DialogueBaseClass.cs
+++ Assets/Scripts/DialogueSystem/DialogueBaseClass.cs
@@ -6,12 +6,16 @@ namespace DialogueSystem
 {
     public class DialogueBaseClass : MonoBehaviour
     {
-        protected IEnumerator WriteText(string input, Text textHolder)
+        protected IEnumerator WriteText(string input, Text textHolder, Color textColor, Font textFont, float delay, AudioClip sound)
         {
+            textHolder.color = textColor;
+            textHolder.font = textFont;
+
             for (int i = 0; i < input.Length; i++)
             {
                 textHolder.text += input[i];
-                yield return new WaitForSeconds(0.1f);
+                SoundMenager.instance.PlaySound(sound);
+                yield return new WaitForSeconds(delay);
             }
         }
     }
