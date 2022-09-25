2562921927153eb073de4bfb1998646460749244
Marcel Kołodziej <kolodziejmarcel1@gmail.com>
2022-09-25 03:04:05 +0200

    do poprawki

--- Assets/Dialog_Holder.cs
+++ Assets/Dialog_Holder.cs
@@ -17,9 +17,10 @@ namespace DialogueSystem
             for (int i = 0; i < transform.childCount; i++)
             {
                // Deactivate();
-                //transform.GetChild(i).gameObject.SetActive(true);
+                transform.GetChild(i).gameObject.SetActive(true);
                 yield return new WaitUntil(() => transform.GetChild(i).GetComponent<DialogueLine>().finished); 
             }
+            gameObject.SetActive(false);
          }
 
 
