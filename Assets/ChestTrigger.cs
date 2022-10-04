50c2d0cb5f745ed41c9e777ee2d58d5ac5d47867
Marcel Kołodziej <kolodziejmarcel1@gmail.com>
2022-10-04 20:02:15 +0200

    commit1

--- Assets/ChestTrigger.cs
+++ Assets/ChestTrigger.cs
@@ -1,18 +1,55 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 public class ChestTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float damage;
 
-    // Update is called once per frame
-    void Update()
+   [Header ("Chest Timers")] 
+   [SerializeField] private float activationDelay;
+   [SerializeField] private float activeTime;
+   private Animator anim;
+   private SpriteRenderer spriteRend;
+
+    [Header ("SFX")]
+    [SerializeField] private AudioClip chestActive;
+
+    private bool triggered;
+    private bool activate;
+
+   private void Awake() {
+    anim = GetComponent<Animator>();
+    spriteRend = GetComponent<SpriteRenderer>();
+   }
+
+   private void OnTriggerEnter2D(Collider2D collision)
+   {
+    if(collision.tag == "Player")
     {
-        
+        Debug.Log("Chest Player detected ");
+        // if(!triggered)
+        // {
+        //     StartCoroutine(ActivateFireTrap());
+        // }
+        // if (activate)
+        //     collision.GetComponent<Health>().TakeDamage(damage);
     }
+   }
+//    private IEnumerator ActivateFireTrap()
+//    {
+//          triggered = true;
+//          spriteRend.color = Color.red;
+//          yield return new WaitForSeconds(activationDelay);
+//          SoundMenager.instance.PlaySound(fireTrapSound);
+//          spriteRend.color = Color.white;
+//          activate = true;
+//          anim.SetBool("isActivated", true);
+
+
+//          yield return new WaitForSeconds(activeTime);
+//          activate = false;
+//          triggered = false;
+//          anim.SetBool("isActivated", false);
+
+//    }
 }
