e7424fef19d7441936263b968c1bf58a47ba0711
Marcel Kołodziej <kolodziejmarcel1@gmail.com>
2022-10-11 20:12:55 +0200

    camera

--- Assets/_Animation/Animation Controllers/CinemachineSwitcher.cs
+++ Assets/_Animation/Animation Controllers/CinemachineSwitcher.cs
@@ -2,16 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Cinemachine;
 
 public class CinemachineSwitcher : MonoBehaviour
 {
+  public GameObject cam2;
+
   [SerializeField] private InputAction action;
 
     private Animator anim;
     private bool ChixieCamera = true;
 
+  [SerializeField] private CinemachineVirtualCamera vcam1; // chixie camera
+  [SerializeField] private CinemachineVirtualCamera vcam2; // chixie camera 2 
+
+
+
   private void Awake() {
-    anim = GetComponent<Animator>();
+   // anim = GetComponent<Animator>();
   }
 
   private void OnEnable()
@@ -25,30 +33,49 @@ public class CinemachineSwitcher : MonoBehaviour
   }
 
   private void Start() {
-    action.performed += ctx => SwichState();
-    Debug.Log("camera1");
+    action.performed += _ => SwitchPriority();
+  }
+  private void SwitchPriority()
+  {
+    if(ChixieCamera)
+    {
+      Debug.Log("camera_1");
+      vcam1.Priority = 0;
+      vcam2.Priority = 1;
+    }
+    else if(vcam2) {
+      Debug.Log("camera_2");
+      vcam1.Priority = 1;
+      vcam2.Priority = 0;
+
+    }
+      ChixieCamera = !ChixieCamera;
   }
 
-private void SwichState() {
-    if (ChixieCamera) {
-        Debug.Log("camera1");
-        anim.Play("VirtualCemara_2");
-    }
-    else{
-        anim.Play("Chixie_Camera");
-        Debug.Log("camera_2");
-    }
-        ChixieCamera = !ChixieCamera;
-    }
+    
 
-      void OnTriggerStay2D (Collider other)
-    {
-        Debug.Log("Stay ");
-    }
+// private void SwichState() {
+//     if (ChixieCamera) {
+//         Debug.Log("camera1");
+//         anim.Play("VirtualCemara_2");
+//     }
+//     else{
+//         anim.Play("Chixie_Camera");
+//         Debug.Log("camera_2");
+//     }
+//         ChixieCamera = !ChixieCamera;
+//     }
 
-      void OnTriggerExit2D (Collider other)
-    {
-        Debug.Log("Exit");
-    }
+//       void OnTriggerStay2D (Collider other)
+//     {
+//         Debug.Log("Stay ");
+//     }
+
+//       void OnTriggerExit2D (Collider other)
+//     {
+//         Debug.Log("Exit");
+//     }
+
+
 
 }
